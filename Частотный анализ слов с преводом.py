a3ee17e32830ab150e477f11f5577dd4831f5018
Simon <worcposj44@gmail.com>
2023-04-11 13:58:00 +0200

    Update Частотный анализ слов с преводом.py

--- Частотный анализ слов с преводом.py
+++ Частотный анализ слов с преводом.py
@@ -4,6 +4,7 @@ import re
 from nltk.tokenize import word_tokenize
 from collections import Counter
 from deep_translator import GoogleTranslator
+from tqdm import tqdm
 
 nltk.download('punkt')
 
@@ -18,9 +19,12 @@ text = re.sub(r'[^\w\s]', '', text)
 # токенизация (разбиение текста на слова)
 tokens = word_tokenize(text)
 
+# подсчет количества повторений каждого слова
+word_counts = Counter(tokens)
+
 # перевод каждого слова и сохранение его переводов в словарь
 translations = {}
-for word in set(tokens):
+for word in tqdm(set(tokens), desc="Перевод слов"):
     try:
         # перевод слова на несколько языков
         translations[word] = GoogleTranslator(source='auto', target='ru').translate(word)
@@ -28,10 +32,18 @@ for word in set(tokens):
         # если возникает ошибка при переводе, записываем None
         translations[word] = None
 
-# сохранение результатов в файл формата JSON
-data = {"word_counts": dict(Counter(tokens)), "translations": translations}
+# сортировка слов по убыванию частоты использования
+sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
 
+# создание списка словарей для записи в файл JSON
+data = []
+for word, count in sorted_words:
+    translation = translations.get(word)
+    data.append({"word": word, "count": count, "translation": translation})
+
+# сохранение результатов в файл формата JSON
 with open("result.json", "w", encoding="utf-8") as file:
-    json.dump(data, file, indent=4, ensure_ascii=False)
+    json.dump(data, file, ensure_ascii=False)
 
 print("Результаты сохранены в файле result.json")
+input()
